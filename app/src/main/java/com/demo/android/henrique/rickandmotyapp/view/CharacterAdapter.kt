eca1a4fbb88b4e006d988bddc9d2d1b8bf4208d8
Henrique Santos da Silva <henriquesantos122@gmail.com>
2021-12-04 14:41:18 -0300

    Finished character detail screen

--- app/src/main/java/com/demo/android/henrique/rickandmotyapp/view/CharacterAdapter.kt
+++ app/src/main/java/com/demo/android/henrique/rickandmotyapp/view/CharacterAdapter.kt
@@ -3,7 +3,9 @@ package com.demo.android.henrique.rickandmotyapp.view
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.demo.android.henrique.rickandmotyapp.R
 import com.demo.android.henrique.rickandmotyapp.databinding.ItemCharacterBinding
 import com.demo.android.henrique.rickandmotyapp.model.Character
 import com.squareup.picasso.Picasso
@@ -12,8 +14,13 @@ class CharacterAdapter(private val characters: List<Character>): RecyclerView.Ad
 
     inner class CharacterViewHolder(private val binding: ItemCharacterBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(character: Character) {
+            binding.root.id = character.id
             binding.txtName.text = character.name
             Picasso.get().load(character.image).into(binding.imgCharacter)
+
+            binding.root.setOnClickListener { view ->
+                view.findNavController().navigate(R.id.action_nav_list_character_to_nav_detail_character)
+            }
         }
     }
 
